6e3ad30883afe0482231e025da4b9bb3e5ead9ea
Deepak Pandey <40865954+freakypandit@users.noreply.github.com>
2023-04-25 03:14:19 +0000

    Postgres DB test

---
+++ Notes.md
@@ -0,0 +1,7 @@
+1. To allow backfill
+airflow dags backfill -s 2023-4-20 -e 2023-4-22 <DAG_ID>
+
+2. Cron Expression Preset
+
+3. Build only one container from docker-compose
+docker-compose up -d --no-deps --build postgres
--- dags/dag_with_cron_expression.py
+++ dags/dag_with_cron_expression.py
@@ -9,18 +9,18 @@ default_args = {
 	'retries': 5,
 	'retry_delay': timedelta(minutes=2)
 }
+
 with DAG (
-	dag_id='DAG_with_catchup_backfill_v01',
+	dag_id='DAG_with_cron_express',
     default_args=default_args,
     description='This is the first dag',
     start_date = datetime(2023, 4, 20, 2),
-    schedule_interval='@daily',
-    catchup=True
+    schedule_interval='0 0 * * *'
 ) as dag:
     
-	task1 = BashOperator(
+    task1 = BashOperator(
 		task_id='FirstTask',
 		bash_command='echo Hello World!'
 	)
 
-	task1
+    task1
