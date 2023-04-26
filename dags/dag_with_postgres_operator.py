ed300bf68361c917756721972b1474e56d0b145b
Deepak Pandey <40865954+freakypandit@users.noreply.github.com>
2023-04-26 04:06:31 +0000

    Postgres Operator Setup part 02

--- dags/dag_with_postgres_operator.py
+++ dags/dag_with_postgres_operator.py
@@ -11,7 +11,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='DAG_with_postgres',
+    dag_id='DAG_with_postgres_v02',
     default_args=default_args,
     start_date=datetime(2023, 4, 24),
     schedule_interval='0 0 * * *'
@@ -20,7 +20,7 @@ with DAG(
         task_id='Create_postgres_table',
         postgres_conn_id='postgres_localhost',
         sql="""
-            create table if not exists dag_runs_test (
+            create table if not exists dag_runs_test_v02 (
                 dt date, 
                 dag_id character varying, 
                 primary key (dt, dag_id)
@@ -28,4 +28,21 @@ with DAG(
         """
     )
 
-    task1
+    task2 = PostgresOperator(
+        task_id='delete_from_table',
+        postgres_conn_id='postgres_localhost',
+        sql= """
+            delete from dag_runs_test_v02 where dt = '{{ ds }}' and dag_id = '{{ dag.dag_id }}'
+        """
+    )
+
+    task3 = PostgresOperator(
+        task_id='insert_into_table',
+        postgres_conn_id='postgres_localhost',
+        sql= """
+            insert into dag_runs_test_v02 (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
+        """
+    )
+
+
+    task1 >> task2 >> task3
